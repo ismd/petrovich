454a34c04e7a773844925a0f63d9c0b9be7e9c4f
Vladimir Kosteley <zzismd@gmail.com>
2015-05-15 20:32:38 +0600

    Главный слайдер (bxslider)

--- index.php
+++ index.php
@@ -5,11 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Петрович</title>
 
-        <link href="/css/normalize.css" rel="stylesheet" />
-        <link href="/css/fotorama.css" rel="stylesheet" />
         <link href="/css/style.css" rel="stylesheet" />
 
         <script src="/js/jquery-2.1.4.min.js"></script>
+        <script src="/js/jquery.bxslider.min.js"></script>
         <script src="/js/fotorama.js"></script>
         <script src="/js/main.js"></script>
     </head>
@@ -41,17 +40,92 @@
             </header>
 
             <div class="main-slider">
-                <div class="main-slider__slide"
-                     style="background-image: url('/pictures/main/slide-1.jpg');">
-                    <div class="main-slider__background">
-                        <blockquote class="main-slider__quote quote">
-                            <strong>Я&nbsp;работаю в&nbsp;этой компании, потому что здесь есть возможность расти</strong>
-                            Можно устроиться, напрмер, оператором, и&nbsp;вырасти до&nbsp;администратора. Насклоько я&nbsp;знаю, половина &laquo;топов&raquo; компании когда-то устраивались на&nbsp;базовые должности.
-                            <i class="quote__close"></i>
-                        </blockquote>
+                <div class="main-bxslider">
+                    <div class="main-slider__slide"
+                         style="background-image: url('/pictures/main/slide-1.jpg');">
+                        <div class="main-slider__background">
+                            <blockquote class="main-slider__quote quote">
+                                <strong>Я&nbsp;работаю в&nbsp;этой компании, потому что здесь есть возможность расти</strong>
+                                Можно устроиться, напрмер, оператором, и&nbsp;вырасти до&nbsp;администратора. Насклоько я&nbsp;знаю, половина &laquo;топов&raquo; компании когда-то устраивались на&nbsp;базовые должности.
+                                <i class="quote__close"></i>
+                            </blockquote>
 
-                        <div class="main-slider__author">Андрей Иванов</div>
+                            <div class="main-slider__author">Андрей Иванов</div>
+                        </div>
                     </div>
+
+                    <div class="main-slider__slide"
+                         style="background-image: url('/pictures/main/slide-1.jpg');">
+                        <div class="main-slider__background">
+                            <blockquote class="main-slider__quote quote">
+                                <strong>Я&nbsp;работаю в&nbsp;этой компании, потому что здесь есть возможность расти</strong>
+                                Можно устроиться, напрмер, оператором, и&nbsp;вырасти до&nbsp;администратора. Насклоько я&nbsp;знаю, половина &laquo;топов&raquo; компании когда-то устраивались на&nbsp;базовые должности.
+                                <i class="quote__close"></i>
+                            </blockquote>
+
+                            <div class="main-slider__author">Андрей Иванов</div>
+                        </div>
+                    </div>
+
+                    <div class="main-slider__slide"
+                         style="background-image: url('/pictures/main/slide-1.jpg');">
+                        <div class="main-slider__background">
+                            <blockquote class="main-slider__quote quote">
+                                <strong>Я&nbsp;работаю в&nbsp;этой компании, потому что здесь есть возможность расти</strong>
+                                Можно устроиться, напрмер, оператором, и&nbsp;вырасти до&nbsp;администратора. Насклоько я&nbsp;знаю, половина &laquo;топов&raquo; компании когда-то устраивались на&nbsp;базовые должности.
+                                <i class="quote__close"></i>
+                            </blockquote>
+
+                            <div class="main-slider__author">Андрей Иванов</div>
+                        </div>
+                    </div>
+
+                    <div class="main-slider__slide"
+                         style="background-image: url('/pictures/main/slide-1.jpg');">
+                        <div class="main-slider__background">
+                            <blockquote class="main-slider__quote quote">
+                                <strong>Я&nbsp;работаю в&nbsp;этой компании, потому что здесь есть возможность расти</strong>
+                                Можно устроиться, напрмер, оператором, и&nbsp;вырасти до&nbsp;администратора. Насклоько я&nbsp;знаю, половина &laquo;топов&raquo; компании когда-то устраивались на&nbsp;базовые должности.
+                                <i class="quote__close"></i>
+                            </blockquote>
+
+                            <div class="main-slider__author">Андрей Иванов</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="main-slider__pager">
+                    <a href="javascript:void(0)"
+                       class="main-slider__pager-item"
+                       data-slide-index="0"
+                       style="background-image: url('/pictures/main/slider-pager-1.png');">
+                        <strong>Анастасия Жаркова</strong><br />
+                        <i>Кассир</i>
+                    </a>
+
+                    <a href="javascript:void(0)"
+                       class="main-slider__pager-item"
+                       data-slide-index="1"
+                       style="background-image: url('/pictures/main/slider-pager-2.png');">
+                        <strong>Светлана Родионова</strong><br />
+                        <i>Старший бухгалтер</i>
+                    </a>
+
+                    <a href="javascript:void(0)"
+                       class="main-slider__pager-item"
+                       data-slide-index="2"
+                       style="background-image: url('/pictures/main/slider-pager-3.png');">
+                        <strong>Андрей Иванов</strong><br />
+                        <i>Логист</i>
+                    </a>
+
+                    <a href="javascript:void(0)"
+                       class="main-slider__pager-item"
+                       data-slide-index="3"
+                       style="background-image: url('/pictures/main/slider-pager-4.png');">
+                        <strong>Мария Яшкова</strong><br />
+                        <i>Администратор</i>
+                    </a>
                 </div>
             </div>
 

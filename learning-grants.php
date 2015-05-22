fe8325f77c841a6346be062a2365f510f5f5942a
Vladimir Kosteley <zzismd@gmail.com>
2015-05-22 16:40:15 +0600

    Контент на страницах обучения

--- learning-grants.php
+++ learning-grants.php
@@ -4,4 +4,22 @@
     <img src="/pictures/learning/grants.jpg" alt="" />
 </div>
 
+<div class="layout">
+    <div class="text-content">
+        <div class="text-content__page">
+            <p>Согласитесь, сложно в&nbsp;18&nbsp;лет точно определить, в&nbsp;какой области ты&nbsp;хочешь учиться и&nbsp;работать. Очень часто это понимание приходит уже с&nbsp;опытом, когда осознаешь&nbsp;&mdash; вот оно, моё, именно здесь мне нужны профессиональные знания. Именно в&nbsp;этом случае, Вы&nbsp;имеете полное право попросить у&nbsp;компании частично или полностью компенсировать Ваше обучение в&nbsp;ВУЗе или другом учебном заведении.</p>
+            <p>Если специалист докажет, что эти знания помогут ему в&nbsp;профессиональном плане, есть большая вероятность того, что он&nbsp;получит грант.</p>
+        </div>
+
+        <div class="text-content__center spacing-text">Студенты</div>
+
+        <div class="fotorama"
+             data-nav="thumbs"
+             data-auto="false">
+            <img src="/pictures/culture/photo-1.jpg" />
+            <img src="/pictures/culture/photo-2.jpg" />
+        </div>
+    </div>
+</div>
+
 <? require '_footer.php' ?>

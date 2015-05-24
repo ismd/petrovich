af44604bae9070ae31a004b80366b4d5becc629c
Vladimir Kosteley <zzismd@gmail.com>
2015-05-24 18:06:02 +0600

    Страница университета

--- learning-university.php
+++ learning-university.php
@@ -1,7 +1,55 @@
 <? require '_header.php' ?>
 
-<div class="big-image">
+<div class="big-image big-image--gradient-yes">
     <img src="/pictures/learning/university.jpg" alt="" />
+    <div class="big-image__text big-image__text--type-big">Это образование, ориентированное на&nbsp;практику. Преподаватели&nbsp;&mdash; руководители компании, передают свои знания и&nbsp;накопленный опыт.</div>
+    <img src="/pictures/learning/university-text.png"
+         class="big-image__text-image"
+         alt="" />
+</div>
+
+<div class="layout">
+    <div class="about-mission about-mission--bottom-border-no">
+        <div class="about-mission__text">
+            <h2 class="about-mission__title">Для тех, кто нацелен на карьеру</h2>
+
+            Диплом Корпоративного университета, помимо знаний, дает приоритет перед другими кандидатами при назначении на&nbsp;руководящую должность. Само поступление в&nbsp;Корпоравтивный университет означает ваше желание карьерного роста и&nbsp;приближает вас к&nbsp;нему
+        </div>
+    </div>
+
+    <div class="learning-info">
+        <h4 class="learning-info__title">Обучение проходит по следующим дисциплинам</h4>
+
+        <div class="learning-info-table">
+            <div class="learning-info-table__column">
+                <div class="learning-info-table__item">Управление человеческими ресурсами</div>
+                <div class="learning-info-table__item">Управление изменениями</div>
+                <div class="learning-info-table__item">Стандарты эффективного руководства</div>
+            </div>
+
+            <div class="learning-info-table__column">
+                <div class="learning-info-table__item">Бизнес риторика</div>
+                <div class="learning-info-table__item">Эффективное взаимодействие с клиентом в сложных ситуациях</div>
+                <div class="learning-info-table__item">Закупки</div>
+                <div class="learning-info-table__item">Гражданское право</div>
+            </div>
+
+            <div class="learning-info-table__column">
+                <div class="learning-info-table__item">Основы бухгалтерского учета</div>
+                <div class="learning-info-table__item">Финансы и кредит</div>
+                <div class="learning-info-table__item">Маркетинг</div>
+                <div class="learning-info-table__item">Всеобщее управление качеством</div>
+            </div>
+        </div>
+
+        <div class="learning-info-description">
+            <img src="/pictures/learning/university-man.jpg" alt="" />
+
+            <div class="learning-info-description__text">
+                Помимо традиционных лекций, преподаватели используют прогрессивные методики обучения, поэтому вы&nbsp;лучше усваиваете информацию и&nbsp;развиваете навыки Александр Петрович, руководитель учебного центра
+            </div>
+        </div>
+    </div>
 </div>
 
 <? require '_footer.php' ?>
